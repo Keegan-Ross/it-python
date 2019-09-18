56ecfa24b76d5ba0152ccbb8ce1dcc9a8b6419d1
Keegan Ross <keeganmross@gmail.com>
2019-09-18 09:56:40 -0400

    Zip code 1 hr

---
+++ zipcode.py
@@ -0,0 +1,15 @@
+from banner import banner
+
+banner("ZIP CODE SORTER", "Keegan Ross")
+print("Welcome to the Newaygo County zip code sorter")
+zip_code = {
+    "49309",
+    "49312",
+    "49337",
+    "49412",
+    "49413",
+    "49327",
+    "49349"
+}
+input("Please enter a zip code: ")
+print(f"The zip code {zip_code} if for")
